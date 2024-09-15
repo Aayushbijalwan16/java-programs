1edf27ed840fc06812b90f47fb27ffa61ffea74c
Aayush bijalwan <101926574+Aayushbijalwan16@users.noreply.github.com>
2024-09-16 02:02:40 +0530

    Update

---
+++ Var.java
@@ -0,0 +1,18 @@
+public class Var {
+             public static void main(String a[]) 
+             {
+                int num1 = 69;  //num is variable and int is data type
+                int num2 = 31;
+                System.out.println(num1 + num2);
+             }    
+}
+//Printing using 3rd variable
+   class Var1{
+    public static void main(String a[]) 
+    {
+       int num1 = 69;  //num is variable and int is data type
+       int num2 = 32;
+       int result = num1 + num2;
+       System.out.println(result);
+      }    
+   }
