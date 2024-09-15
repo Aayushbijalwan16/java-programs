40e075202a53590a7afda30539737903d54a0cf2
Aayush bijalwan <101926574+Aayushbijalwan16@users.noreply.github.com>
2024-09-16 02:47:31 +0530

    update

---
+++ Dtypes.java
@@ -0,0 +1,21 @@
+public class Dtypes {
+    public static void main(String a[])
+    { //Data types in java 
+        int num = 10;
+        float f = 6.5f; 
+        byte  by = 32;
+        long l = 654161l;
+        double d = 2.5d;
+        char ch = 'A';
+        boolean b = true;
+        short sh = 4654;
+        System.out.println(num);
+        System.out.println(f);
+        System.out.println(by);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(b);
+        System.out.println(sh);
+    }
+}
