a1da44d9dbf43fc1808c7e73f477059b18b19865
Aayush bijalwan <101926574+Aayushbijalwan16@users.noreply.github.com>
2024-09-21 23:58:28 +0530

    update

---
+++ dowhile.java
@@ -0,0 +1,10 @@
+// Loop Satement do-while loop
+public class dowhile {
+    public static void main(String[] args){
+       int i = 5;
+       do{
+        System.out.println("Jai Shree Ram");
+        i++;
+       }while(i<=4);
+    }
+}
