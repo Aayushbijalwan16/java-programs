9d8c101de96eb58809df69d17ef55960b6ef810d
Aayush bijalwan <101926574+Aayushbijalwan16@users.noreply.github.com>
2024-09-23 23:48:30 +0530

    update

--- for.java
+++ for.java
@@ -1,6 +1,12 @@
-public class for {
+// Ex of Loop statement for loop with nested for loop 
+public class For {
     public static void main(String[] args) {
-
-    
+     
+        for(int i=1; i<=7 ; i++){
+            System.out.println("Day" + i);
+            for(int j=1; j<=9; j++){
+                System.out.println("" + (j+8) + " - " + (j+9));
+            }
+        }
     }
 }
