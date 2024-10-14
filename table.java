9dc054297a100650e5b53b05979b8a343958513d
Aayush bijalwan <101926574+Aayushbijalwan16@users.noreply.github.com>
2024-10-14 13:10:40 +0530

    Example of For Loop

---
+++ table.java
@@ -0,0 +1,12 @@
+// Examole of For loop
+import java.util.*;
+public class table{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int n = sc.nextInt();
+        for (int i=1; i<=10; i++){
+            System.out.println(n + " x " + i + " = " + (n*i));
+        }
+    }
+}
